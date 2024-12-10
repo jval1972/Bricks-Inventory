d8095c8a5f33f2ccfb855de1bcaa283be7c05122
jval1972 <jvalavanis@gmail.com>
2024-12-10 06:37:55 +0200

    Uptime crawler fix

--- bi_crawler.pas
+++ bi_crawler.pas
@@ -2384,7 +2384,7 @@ begin
   end;
 
   if (myMonth <= 3) or (myMonth > 10) then
-    if myHour = 7 then
+    if myHour = 8 then
       if (myMin > 49) or ((myMin = 49) and (mySec > 57)) then
       begin
         Result := False;
@@ -2392,7 +2392,7 @@ begin
       end;
 
   if (myMonth > 3) and (myMonth <= 10) then
-    if myHour = 8 then
+    if myHour = 7 then
       if (myMin > 49) or ((myMin = 49) and (mySec > 57)) then
       begin
         Result := False;
